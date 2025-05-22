6130b5eb1871f408bbfb80cad0371163118d9dba
Lebohang Mofokeng <lp.mofokeng96@gmail.com>
2025-05-22 15:59:11 +0200

    created a component

---
+++ src/app/following-post/following-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-following-post',
+  imports: [],
+  templateUrl: './following-post.component.html',
+  styles: ``
+})
+export class FollowingPostComponent {
+
+}
