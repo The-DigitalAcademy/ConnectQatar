db88c03e58f4b64bbc033027302956dac8d1a78c
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-05-22 22:49:38 +0200

    created simple components and a service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ViewPostsComponent } from './view-posts/view-posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '**', redirectTo: 'user', pathMatch: 'full' },
+    {path: 'user', component: ViewPostsComponent}
+];
---
+++ src/app/view-posts/view-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-posts',
+  imports: [],
+  templateUrl: './view-posts.component.html',
+  styles: ``
+})
+export class ViewPostsComponent {
+
+}
