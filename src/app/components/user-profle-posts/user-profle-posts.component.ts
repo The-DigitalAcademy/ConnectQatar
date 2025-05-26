5cc510de2539a5ec04cb2418a87eb3a05e7123c7
Bongeka28 <bongekamdluli17@gmail.com>
2025-05-26 11:27:51 +0200

    edited user profile bottom nav and posts

--- src/app/components/user-profle-posts/user-profle-posts.component.ts
+++ src/app/components/user-profle-posts/user-profle-posts.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from '../post-card/post-card.component';
 
 @Component({
   selector: 'app-user-profle-posts',
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './user-profle-posts.component.html',
   styleUrl: './user-profle-posts.component.css'
 })
