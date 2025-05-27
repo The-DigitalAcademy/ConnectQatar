d08e6ea54e95ed7ab58a5d608e15cb8e53fad403
Phume Dlamini <phumelele.dlamini1000@gmail.com>
2025-05-27 11:44:42 +0200

    add overlay functionality to create-posts component for story and post creation

--- src/app/components/create-posts/create-posts.component.ts
+++ src/app/components/create-posts/create-posts.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-posts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-posts.component.html',
   styleUrl: './create-posts.component.css'
 })
 export class CreatePostsComponent {
+  showOverlay: boolean = false
+  overlayContent: string = ''
 
   createStory(){
     console.log('I work')
+    this.overlayContent = 'New Story'
+    this.showOverlay = true
   }
 
   createPost(){
     console.log('I work too')
+    this.overlayContent = 'New Post'
+    this.showOverlay = true
+  }
+
+  closeOverlay(){
+    this.showOverlay = false;
+    this.overlayContent = '';
   }
 }
