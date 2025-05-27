a7cbf6c8aa3602cd4aa064fcbecc6eeef463157f
Phume Dlamini <phumelele.dlamini1000@gmail.com>
2025-05-27 13:18:33 +0200

    edited overlay functionality in create-posts component with form for image upload, image preview and caption

--- src/app/components/create-posts/create-posts.component.ts
+++ src/app/components/create-posts/create-posts.component.ts
@@ -1,30 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-posts',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-posts.component.html',
   styleUrl: './create-posts.component.css'
 })
 export class CreatePostsComponent {
   showOverlay: boolean = false
+  isStory: boolean = false
   overlayContent: string = ''
+  selectedImageUrl: string | ArrayBuffer | null = null
 
   createStory(){
     console.log('I work')
     this.overlayContent = 'New Story'
+    this.isStory = true
     this.showOverlay = true
   }
 
   createPost(){
     console.log('I work too')
     this.overlayContent = 'New Post'
+    this.isStory = false
     this.showOverlay = true
   }
 
   closeOverlay(){
     this.showOverlay = false;
     this.overlayContent = '';
+  }
+
+  onSubmit() {
+    console.log('Form submitted');
+    this.closeOverlay();
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.selectedImageUrl = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
   }
 }
