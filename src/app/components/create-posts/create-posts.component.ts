89bdb8f0a9be03daedcb2ae4a7e0f710941a709c
Phume Dlamini <phumelele.dlamini1000@gmail.com>
2025-05-27 09:02:15 +0200

    add create-posts component with basic template and styles

---
+++ src/app/components/create-posts/create-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-posts',
+  imports: [],
+  templateUrl: './create-posts.component.html',
+  styleUrl: './create-posts.component.css'
+})
+export class CreatePostsComponent {
+
+}
