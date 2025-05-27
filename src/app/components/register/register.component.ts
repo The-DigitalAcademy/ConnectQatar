a63507277a1077a6335c950351f88dc2ec732606
Dineo <Dineo>
2025-05-27 14:18:35 +0200

    login and register functionality added

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService, User } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  signupForm: FormGroup;
+  message = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signupForm = this.fb.group(
+      {
+        fullname: ['', Validators.required],
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    return group.get('password')!.value === group.get('confirmPassword')!.value
+      ? null
+      : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      const { confirmPassword, ...user } = this.signupForm.value;
+      this.authService.signup(user as User).subscribe({
+        next: () => {
+          this.message = 'Signup successful! Redirecting to login...';
+          setTimeout(() => this.router.navigate(['/login']), 2000);
+        },
+        error: () => (this.message = 'Signup failed. Try again.'),
+      });
+    } else {
+      this.message = 'Form invalid or passwords do not match.';
+    }
+  }
 
 }
