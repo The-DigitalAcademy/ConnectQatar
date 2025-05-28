7505a35fea17bfb7f14d924dda1d14a9550dd4df
DA M1 110 <dam1110@110-DAs-MacBook-Air.local>
2025-05-28 13:19:12 +0200

    displaying all users and posts

--- src/app/components/suggested-post-card/suggested-post-card.component.ts
+++ src/app/components/suggested-post-card/suggested-post-card.component.ts
@@ -1,32 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service';
-import { CommonModule } from '@angular/common';
+
+interface Post {
+  id: string;
+  title: string;
+  image: string;
+  userId: string;
+}
+
+interface User {
+  id: string;
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  profileImage: string;
+}
 
 @Component({
   selector: 'app-suggested-post-card',
   imports: [CommonModule],
   templateUrl: './suggested-post-card.component.html'
 })
+
 export class SuggestedPostCardComponent implements OnInit {
-  users: any[] = [];
-  posts: any[] = [];
-  usersWithPosts: any[] = [];
-user: any;
+  users: User[] = [];
+  posts: Post[] = [];
+  usersWithPosts: Array<User & { posts: Post[] }> = [];
 
   constructor(private postService: PostService) {}
 
   ngOnInit() {
-    // all users and posts
     Promise.all([
       this.postService.getAllUsers().toPromise(),
       this.postService.getPostsWithDetails().toPromise()
     ]).then(([users, postsWithDetails]) => {
-     
       const posts = postsWithDetails?.map((item: any) => item.post);
-
       this.users = users || [];
       this.posts = posts || [];
-
       this.usersWithPosts = this.users.map(user => ({
         ...user,
         posts: this.posts.filter(post => String(post.userId) === String(user.id))
