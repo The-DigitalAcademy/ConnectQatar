6f86015b854c8ffc9d6cb7b8fb5abdb8e94ae217
phumebyte <phumelele.dlamini1000@gmail.com>
2025-05-30 00:19:19 +0200

    Update register and login components for naming consistency with minor UI and message changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,6 @@ import { AuthService } from '../services/auth.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
   loginForm: FormGroup;
   message = '';
 
@@ -34,7 +33,7 @@ export class LoginComponent {
       const { username, password } = this.loginForm.value;
       this.authService.login(username, password).subscribe((success) => {
         if (success) {
-          this.message = 'Login successful! Redirecting...';
+          this.message = 'Successfully Signed In! Redirecting...';
           setTimeout(() => this.router.navigate(['/posts']), 1000);
         } else {
           this.message = 'Invalid username or password.';
