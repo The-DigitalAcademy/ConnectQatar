2bd277aeb2d4e43668343bca5864d7171d847bf6
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-05-30 12:57:00 +0200

    made ui consistent

--- src/app/Pages/user-profile/user-profile.component.ts
+++ src/app/Pages/user-profile/user-profile.component.ts
@@ -4,10 +4,11 @@ import { UserProflePostsComponent } from '../../components/user-profle-posts/use
 import { UserpHeaderComponent } from '../../components/userp-header/userp-header.component';
 import { BottomNavComponent } from '../../components/bottom-nav/bottom-nav.component';
 import { RouterLink } from '@angular/router';
+import { PostCardComponent } from "../../components/post-card/post-card.component";
 
 @Component({
   selector: 'app-user-profile',
-  imports: [UserProfleButtonsComponent, UserProflePostsComponent, UserpHeaderComponent, BottomNavComponent],
+  imports: [UserProfleButtonsComponent, UserpHeaderComponent, BottomNavComponent, PostCardComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
