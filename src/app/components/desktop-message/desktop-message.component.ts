fd4ba70f5ec932fa40f45bec5e6fd14e7c5ee9f2
phumebyte <phumelele.dlamini1000@gmail.com>
2025-05-30 16:04:17 +0200

    add responsive desktop message component for non-mobile/tablet users

---
+++ src/app/components/desktop-message/desktop-message.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-message',
+  imports: [],
+  templateUrl: './desktop-message.component.html',
+  styleUrl: './desktop-message.component.css'
+})
+export class DesktopMessageComponent {
+
+}
