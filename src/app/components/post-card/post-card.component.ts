6de4168f0abffd224566ed3c495379ca6d1e3f52
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-06-20 21:13:12 +0200

    added unfollow button

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,9 +1,10 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-post-card',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './post-card.component.html',
 })
 export class PostCardComponent {
